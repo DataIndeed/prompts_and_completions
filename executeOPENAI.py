22c844a6afdb1f250cd31ed1907083b2156291dc
Xi Zeng <zengxiliang@gmail.com>
2023-04-23 09:41:26 -0400

    FUNWITHGPT
    FUNWITHGPT

---
+++ executeOPENAI.py
@@ -0,0 +1,8 @@
+import os
+import openai
+openai.organization = "org-yPGTuYlHnnpECjESl9iCKgD1"
+openai.api_key = "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"
+print(openai.Model.list())
+
+
+
